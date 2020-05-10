6e148244e67b16b6193e85196aa82d29301f6ea1
Eduardo Castro <eduardocastrogt@gmail.com>
2020-05-10 00:11:36 -0600

    Table for store books

--- postgreSQL/query.sql
+++ postgreSQL/query.sql
@@ -11,43 +11,42 @@ create table Tbl_User
     Pass text not null --will be crypt
 );
 
+create table Tbl_Book
+(
+    Id_Book serial primary key,
+    ISBN text not null,
+    Title text not null,
+    Author text not null,
+    Year int not null
+);
+
 --Function for store User information 
-create or replace function Book_SaveUser
-(name_user varchar(50), lastname_user varchar(50), email_user varchar(75), pass_user text)
-returns void
-as $$
+create or replace function Book_SaveUser (name_user varchar(50), lastname_user varchar(50), email_user varchar(75), pass_user text) returns void as $$
     begin
-        insert into Tbl_User(Name, LastName, Email, Pass) 
+        insert into Tbl_User(Name, LastName, Email, Pass)
         values (upper(name_user), upper(lastname_user), lower(email_user), crypt(pass_user, gen_salt('bf')));
     end
 $$ language plpgsql;
 
 --Function ExistUser (for login)
-create or replace function Book_ExistUser
-(email_user varchar(75), user_password text)
-returns int 
-as $$
-declare exist_user int;
-    begin
-        select 
-            Id_User into exist_user
+create or replace function Book_ExistUser (email_user varchar(75), user_password text) returns table(user_id int, user_name text) as $$
+        select
+            Id_user as user_id,-- into user
+            Name || ' ' || LastName as user_name
         from
             Tbl_User
-        where 
+        where
             Email = email_user
             and Pass = crypt(user_password, Pass);
-
-        return exist_user;
-    end
-$$ language plpgsql
-
-
+$$ language sql
 
 --select public.Book_SaveUser('eduardo', 'castro', 'eduardocastrogt@gmail.co', 'umg');
 
---select public.Book_ExistUser('eduardocastrogt@gmail.com', 'umg');
+--select  * from public.Book_ExistUser('eduardocastrogt@gmail.co', 'umg');
 
 --select * from public.Tbl_User
 
 --truncate table Tbl_User
+
+
 
